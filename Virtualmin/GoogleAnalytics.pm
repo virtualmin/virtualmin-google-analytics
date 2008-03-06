bee6efa34ca8f6026223b97f8a01833230cb44a1
Jamie Cameron <jcameron@webmin.com>
2008-03-06 02:26:52 +0000

    Use new analytics code

--- Virtualmin/GoogleAnalytics.pm
+++ Virtualmin/GoogleAnalytics.pm
@@ -44,7 +44,7 @@ sub handler {
     # Work out the script we want to add
     my $addscript;
     if ($account) {
-      $addscript .= "<script src=\"http://www.google-analytics.com/urchin.js\" type=\"text/javascript\"></script> <script type=\"text/javascript\">_uacct = \"$account\"; urchinTracker();</script>";
+      $addscript .= "<script type=\"text/javascript\">var gaJsHost = ((\"https:\" == document.location.protocol) ? \"https://ssl.\" : \"http://www.\"); document.write(unescape(\"%3Cscript src='\" + gaJsHost + \"google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E\")); </script> <script type=\"text/javascript\"> var pageTracker = _gat._getTracker(\"$account\"); pageTracker._initData(); pageTracker._trackPageview();</script>";
     }
     if ($mybloglog) {
       $addscript .= "<script type=\"text/javascript\" src=\"http://track3.mybloglog.com/js/jsserv.php?mblID=$mybloglog\"></script>";
