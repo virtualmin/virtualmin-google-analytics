665f1270ba4cb861bd7501269372547afc6446e5
Jamie Cameron <jcameron@webmin.com>
2008-05-05 04:33:31 +0000

    Piwik url tweaks

--- Virtualmin/GoogleAnalytics.pm
+++ Virtualmin/GoogleAnalytics.pm
@@ -58,7 +58,7 @@ sub handler {
       $addscript .= "<script src=\"http://static.getclicky.com/$clicky.js\" type=\"text/javascript\"></script><noscript><p><img alt=\"Clicky\" src=\"http://static.getclicky.com/${clicky}ns.gif\"/></p></noscript>";
     }
     if ($piwik) {
-      $addscript .= "<!-- Piwik --><a href=\"http://piwik.org\" title=\"Web analytics\" onclick=\"window.open(this.href);return(false);\"><script language=\"javascript\" src=\"$piwik_url/piwik.js\" type=\"text/javascript\"></script><script type=\"text/javascript\"><!-- piwik_action_name = ''; piwik_idsite = $piwik; piwik_url = '$piwik_url/piwik.php'; piwik_log(piwik_action_name, piwik_idsite, piwik_url); //--> </script><object> <noscript><p>Web analytics <img src=\"$piwik_url/piwik.php\" style=\"border:0\" alt=\"piwik\"/></p> </noscript></object></a>";
+      $addscript .= "<a href=\"http://piwik.org\" title=\"Web analytics\" onclick=\"window.open(this.href);return(false);\"><script language=\"javascript\" src=\"$piwik_url/piwik.js\" type=\"text/javascript\"></script><script type=\"text/javascript\">piwik_action_name = ''; piwik_idsite = $piwik; piwik_url = '$piwik_url/piwik.php'; piwik_log(piwik_action_name, piwik_idsite, piwik_url);</script><object> <noscript><p>Web analytics <img src=\"$piwik_url/piwik.php\" style=\"border:0\" alt=\"piwik\"/></p> </noscript></object></a>";
 <!-- /Piwik -->
     }
   
