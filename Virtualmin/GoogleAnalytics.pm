541beb3c352ac22aac0763034be008e0c8ceb068
Jamie Cameron <jcameron@webmin.com>
2008-12-13 20:39:11 +0000

    New google code

--- Virtualmin/GoogleAnalytics.pm
+++ Virtualmin/GoogleAnalytics.pm
@@ -46,7 +46,7 @@ sub handler {
     # Work out the script we want to add
     my $addscript;
     if ($account) {
-      $addscript .= "<script type=\"text/javascript\">var gaJsHost = ((\"https:\" == document.location.protocol) ? \"https://ssl.\" : \"http://www.\"); document.write(unescape(\"%3Cscript src='\" + gaJsHost + \"google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E\")); </script> <script type=\"text/javascript\"> var pageTracker = _gat._getTracker(\"$account\"); pageTracker._initData(); pageTracker._trackPageview();</script>";
+      $addscript .= "<script type=\"text/javascript\">var gaJsHost = ((\"https:\" == document.location.protocol) ? \"https://ssl.\" : \"http://www.\"); document.write(unescape(\"%3Cscript src='\" + gaJsHost + \"google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E\")); </script> <script type=\"text/javascript\"> try { var pageTracker = _gat._getTracker(\"$account\"); pageTracker._trackPageview(); } catch(err) { }</script>";
     }
     if ($mybloglog) {
       $addscript .= "<script type=\"text/javascript\" src=\"http://track3.mybloglog.com/js/jsserv.php?mblID=$mybloglog\"></script>";
